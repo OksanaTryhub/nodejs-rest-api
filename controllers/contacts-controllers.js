b22b783ef62b1e24845f3bdedff8517cbb068ff3
OksanaTryhub <ksanatrigub@gmail.com>
2023-04-08 12:54:09 +0300

    add MongoDB

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,16 +1,16 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 
 const { HttpError } = require("../helpers");
 const ctrlWrapper = require("../utils/ctrlWrapper");
 
-const getAllContacts = async (req, res, next) => {
-  const result = await contacts.listContacts();
+const getAllContacts = async (req, res) => {
+  const result = await Contact.find();
   res.status(200).json(result);
 };
 
-const getContactById = async (req, res, next) => {
+const getContactById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   console.log(result);
   if (!result) {
     throw HttpError(404);
@@ -18,14 +18,14 @@ const getContactById = async (req, res, next) => {
   res.status(200).json(result);
 };
 
-const addContact = async (req, res, next) => {
-  const result = await contacts.addContact(req.body);
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const deleteContactById = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndDelete(contactId);
   if (!result) {
     throw HttpError(404);
   }
@@ -34,13 +34,22 @@ const deleteContactById = async (req, res, next) => {
   });
 };
 
-const updateContactById = async (req, res, next) => {
+const updateContactById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
   if (!result) {
     throw HttpError(404);
   } else if (Object.keys(req.body).length === 0) {
     throw HttpError(400, "missing fields");
+  }
+  res.status(200).json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404);
   }
   res.status(200).json(result);
 };
@@ -51,4 +60,5 @@ module.exports = {
   addContact: ctrlWrapper(addContact),
   deleteContactById: ctrlWrapper(deleteContactById),
   updateContactById: ctrlWrapper(updateContactById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-//avEF13kPXKUNp4Hv
 
-const DB_HOST =
-  "mongodb+srv://Oksana:avEF13kPXKUNp4Hv@cluster0.28dycdh.mongodb.net/contact_book?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() =>
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Database connection successful");
     })
   )
